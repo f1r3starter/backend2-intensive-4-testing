de938dea49aaf24fe9705ced5cb8a26d77eb04e8
f1r3starter <andrey.filenochko@gmail.com>
2019-08-07 19:05:23 +0300

    classes

--- source/odm/classes.js
+++ source/odm/classes.js
@@ -1,24 +1,61 @@
 // Core
 import mongoose from 'mongoose';
+import v4 from 'uuid/v4';
 
-const classesSchema = new mongoose.Schema({
-    title:       String,
-    description: String,
-    hash:        String,
-    students:    [
-        {
-            user:     ObjectID,
-            status:   String,
-            expelled: Boolean,
-            notes:    String,
-        },
-    ],
-    lessons: [
-        {
-            lesson:    ObjectID,
-            scheduled: Date,
-        },
-    ],
-});
+// Instruments
+import { users, lessons } from './';
 
-export const classes = mongoose.model('classes', classesSchema);
+const schema = new mongoose.Schema(
+    {
+        hash: {
+            type:     String,
+            required: true,
+            unique:   true,
+            default:  () => v4(),
+        },
+        title:       String,
+        description: String,
+        students:    [
+            {
+                user: {
+                    type: mongoose.SchemaTypes.ObjectId,
+                    ref:  users,
+                },
+                status: {
+                    type: String,
+                    enum: [ 'standard', 'select', 'premium' ],
+                },
+                expelled: Boolean,
+                notes:    String,
+            },
+        ],
+        lessons: [
+            {
+                lesson: {
+                    type: mongoose.SchemaTypes.ObjectId,
+                    ref:  lessons,
+                },
+                scheduled: Date,
+            },
+        ],
+        duration: {
+            started: {
+                type:     Date,
+                required: true,
+            },
+            closed: {
+                type:     Date,
+                required: true,
+            },
+        },
+        order: Number,
+    },
+    { timestamp: { createdAt: 'created', updatedAt: 'modified' } },
+);
+
+schema.index({ title: 'text', description: 'text' });
+schema.index({ order: 1 }, { name: 'order' });
+
+export const classes = mongoose.model('classes', schema);
+
+classes.createIndexes();
