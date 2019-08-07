2826397294533abf5ef5e19c1af8758dc8ad696f
f1r3starter <andrey.filenochko@gmail.com>
2019-08-07 19:05:05 +0300

    users

--- source/odm/users.js
+++ source/odm/users.js
@@ -1,39 +1,75 @@
 // Core
 import mongoose from 'mongoose';
+import v4 from 'uuid/v4';
 
-const usersSchema = new mongoose.Schema({
-    name: {
-        first: String,
-        last:  String,
-    },
-    phones: [
-        {
-            email:   String,
-            primary: Boolean,
+const schema = new mongoose.Schema(
+    {
+        hash: {
+            type:     String,
+            required: true,
+            unique:   true,
+            default:  () => v4(),
         },
-    ],
-    emails: [
-        {
-            email:   String,
-            primary: Boolean,
+        name: {
+            first: {
+                type:     String,
+                required: true,
+            },
+            last: {
+                type:     String,
+                required: true,
+            },
         },
-    ],
-    password: String,
-    sex:      {
-        type: String,
-        enum: [ 'm', 'f' ],
+        emails: [
+            {
+                email: {
+                    type:     String,
+                    unique:   true,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        phones: [
+            {
+                phone: {
+                    type:     String,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        password: {
+            type:     String,
+            select:   false,
+            required: true,
+        },
+        sex: {
+            type:     String,
+            enum:     [ 'm', 'f' ],
+            required: true,
+        },
+        roles: [
+            {
+                type:    String,
+                default: 'newbie',
+                enum:    [ 'newbie', 'student', 'teacher' ],
+            },
+        ],
+        socials: {
+            facebook: String,
+            linkedin: String,
+            github:   String,
+            skype:    String,
+        },
+        notes:    String,
+        disabled: Boolean,
     },
-    social: {
-        facebook: String,
-        linkedin: String,
-        github:   String,
-        skype:    String,
-    },
-    notes:    String,
-    hash:     String,
-    disabled: Boolean,
-    created:  Date,
-    modified: Date,
-});
+    { timestamp: { createdAt: 'created', updatedAt: 'modified' } },
+);
+schema.index({ 'name.first': 1, 'name.last': 1 }, { name: 'flName' });
+schema.index({ notes: 'text' }, { name: 'notes' });
 
-export const users = mongoose.model('users', usersSchema);
+export const users = mongoose.model('users', schema);
+
+users.createIndexes();
